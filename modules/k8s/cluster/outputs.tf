77743afe1bf225d4dcf3e6fe5d51a480965900f0
njay4928 <navindu.jayatilake@sysco.com>
2025-02-08 15:18:22 +0530

    update

--- modules/k8s/cluster/outputs.tf
+++ modules/k8s/cluster/outputs.tf
@@ -8,4 +8,5 @@ output "id" {
 
 output "test" {
   value       = module.homelab_cluster
+  sensitive = true
 }
--- modules/k8s/cluster/versions.tf
+++ modules/k8s/cluster/versions.tf
@@ -12,12 +12,12 @@
 #   }
 # }
 
-provider "helm" {
-  kubernetes {
-    host  = module.homelab_cluster.endpoint
-    token = module.homelab_cluster[0].kube_config[0].token
-    cluster_ca_certificate = base64decode(
-      module.homelab_cluster[0].kube_config[0].cluster_ca_certificate
-    )
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host  = module.homelab_cluster.endpoint
+#     token = module.homelab_cluster[0].kube_config[0].token
+#     cluster_ca_certificate = base64decode(
+#       module.homelab_cluster[0].kube_config[0].cluster_ca_certificate
+#     )
+#   }
+# }
