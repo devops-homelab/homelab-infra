588d4741230022b2e7854d45736aea9071ae24e8
njay4928 <navindu.jayatilake@sysco.com>
2025-02-09 11:07:24 +0530

    update

--- modules/k8s/cluster/versions.tf
+++ modules/k8s/cluster/versions.tf
@@ -19,10 +19,3 @@ provider "helm" {
     )
   }
 }
-
-provider "kubectl" {
-  host                   = module.homelab_cluster.endpoint[0]
-  cluster_ca_certificate = base64decode(module.homelab_cluster.cluster_ca_certificate[0])
-  token                  = module.homelab_cluster.token[0]
-  load_config_file       = false
-}
