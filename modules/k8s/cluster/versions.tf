a3933ec3430adb0fc1cf342f6cda05c0eb8414c4
njay4928 <navindu.jayatilake@sysco.com>
2025-02-09 09:56:11 +0530

    update

--- modules/k8s/cluster/versions.tf
+++ modules/k8s/cluster/versions.tf
@@ -2,14 +2,12 @@
 ################################################################################
 # Terraform Providers
 ################################################################################
-provider "kubectl" {
-  kubernetes {
+provider "kubernetes" {
     host  = module.homelab_cluster.endpoint[0]
     token = module.homelab_cluster.token[0]
     cluster_ca_certificate = base64decode(
       module.homelab_cluster.cluster_ca_certificate[0]
     )
-  }
 }
 
 provider "helm" {
