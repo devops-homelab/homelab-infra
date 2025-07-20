d3c7e7bcb01dfe3a3032f62daaf5834438b64f1b
njay4928 <njay4928@sysco.com>
2025-07-20 12:46:21 +0530

    update required providers in versions.tf to include kubernetes and helm with specified versions

--- modules/k8s/cluster/versions.tf
+++ modules/k8s/cluster/versions.tf
@@ -30,8 +30,17 @@ provider "kubectl" {
 
 terraform {
   required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11.0"
+    }
     kubectl = {
       source  = "gavinbunney/kubectl"
+      version = "~> 1.14.0"
     }
   }
 }
